db6e1b8579e2aeaab32fa6052b27ac2c2c090fad
Jongheon Park <48527182+hyphmngo@users.noreply.github.com>
2023-12-08 13:43:32 -0500

    Add win/draw condition

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -1,13 +1,13 @@
-#include "event.h"
-#include "sage.h"
-#include "graphics.h"
-#include "malloc.h"
+#include <event.h>
+#include <graphics.h>
+#include <malloc.h>
+#include <sage.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
-#include <bitmap.h>
 
 #define ABS_INT 32767
 
@@ -28,19 +28,21 @@
 #define ROW_LIMIT1 (2 * HEIGHT / 3)
 #define ROW_LIMIT2 (HEIGHT)
 
-#define CELL_X0 (XO_WIDTH / 2)
+#define CELL_X0 (XO_WIDTH / 2) // TODO: Need to account for change in origin
 #define CELL_X1 (CELL_X0 + COL_LIMIT0)
 #define CELL_X2 (CELL_X0 + COL_LIMIT1)
 #define CELL_Y0 (XO_HEIGHT / 2)
 #define CELL_Y1 (CELL_Y0 + ROW_LIMIT0)
 #define CELL_Y2 (CELL_Y0 + ROW_LIMIT1)
 
-int cell_x[3] = {CELL_X0, CELL_X1, CELL_X2};
-int cell_y[3] = {CELL_Y0, CELL_Y1, CELL_Y2};
+int cell_x[3];
+int cell_y[3];
 
+char turn;
 bool filled[9];
 bool which_player[9];
 bool whos_turn;
+bool who_won;
 
 void draw_board(Pixel *buf, Pixel *background_pix, Pixel *border_pix, uint32_t screen_width, uint32_t screen_height) {
   // Background
@@ -131,6 +133,108 @@ void update_cell(int cell, Pixel *buf) {
   printf("Cell %d filled by player %d\n", cell, whos_turn);
   whos_turn = !whos_turn;
   printf("Player %d's turn...\n", whos_turn);
+  ++turn;
+}
+
+// Calculate winning/draw condition, return true if someone won.
+bool calc_win() {
+  char p0_counter = 0;
+  char p1_counter = 0;
+  for (int y = 0; y < 9; y += 3) {
+    for (int x = y; x < y + 3; ++x) {
+      if (filled[x]) {
+        p0_counter = which_player[x] == 0 ? p0_counter + 1 : p0_counter;
+        p1_counter = which_player[x] == 1 ? p1_counter + 1 : p1_counter;
+      }
+    }
+
+    if (p0_counter == 3) {
+      who_won = 0;
+      return true;
+    }
+
+    if (p1_counter == 3) {
+      who_won = 1;
+      return true;
+    }
+
+    p0_counter = 0;
+    p1_counter = 0;
+  }
+
+  for (int i = 0; i < 3; ++i) {
+    for (int j = i; j < i + 7; j += 3) {
+      if (filled[j]) {
+        p0_counter = which_player[j] == 0 ? p0_counter + 1 : p0_counter;
+        p1_counter = which_player[j] == 1 ? p1_counter + 1 : p1_counter;
+      }
+    }
+
+    if (p0_counter == 3) {
+      who_won = 0;
+      return true;
+    }
+
+    if (p1_counter == 3) {
+      who_won = 1;
+      return true;
+    }
+
+    p0_counter = 0;
+    p1_counter = 0;
+  }
+
+  for (int i = 0; i < 9; i += 4) {
+    if (filled[i]) {
+      p0_counter = which_player[i] == 0 ? p0_counter + 1 : p0_counter;
+      p1_counter = which_player[i] == 1 ? p1_counter + 1 : p1_counter;
+    }
+  }
+
+  if (p0_counter == 3) {
+    who_won = 0;
+    return true;
+  }
+
+  if (p1_counter == 3) {
+    who_won = 1;
+    return true;
+  }
+
+  p0_counter = 0;
+  p1_counter = 0;
+
+  for (int i = 2; i < 7; i += 2) {
+    if (filled[i]) {
+      p0_counter = which_player[i] == 0 ? p0_counter + 1 : p0_counter;
+      p1_counter = which_player[i] == 1 ? p1_counter + 1 : p1_counter;
+    }
+  }
+
+  if (p0_counter == 3) {
+    who_won = 0;
+    return true;
+  }
+
+  if (p1_counter == 3) {
+    who_won = 1;
+    return true;
+  }
+
+  p0_counter = 0;
+  p1_counter = 0;
+  
+  return false;
+}
+
+void annouce_winner() {
+  printf("Player %d won!\n", who_won);
+  // TODO: Draw a win screen
+}
+
+void annouce_draw() {
+  printf("It's a draw!\n");
+  // TODO: Draw a draw screen
 }
 
 int main() {
@@ -139,6 +243,16 @@ int main() {
   // printf("~> %p", buf);
   // init_malloc(buf, sizeof(buf));
   // Pixel *pbuf = malloc(1 * 1 * sizeof(Pixel));
+  // pbuf[0] = p;
+  // free(pbuf);
+
+  // Init cell positions
+  cell_x[0] = CELL_X0;
+  cell_x[1] = CELL_X1;
+  cell_x[2] = CELL_X2;
+  cell_y[0] = CELL_Y0;
+  cell_y[1] = CELL_Y1;
+  cell_y[2] = CELL_Y2;
   
   Rectangle screen_rect;
   screen_get_dims(&screen_rect);
@@ -149,21 +263,6 @@ int main() {
   printf("Printing board...\n");
   draw_board(buf, &background_pix, &border_pix, WIDTH, HEIGHT);
   printf("Printed board!\n");
-
-  // Rectangle x_rect = {50, 50, XO_WIDTH, XO_HEIGHT};
-  // Pixel x_color = {255, 0 , 0 , 0};
-  // draw_rect(buf, &x_rect, &x_color, WIDTH, HEIGHT);
-  // Rectangle lol = {0, 0, WIDTH, HEIGHT};
-  // screen_draw_rect(buf, &lol, 1, 1);
-  // screen_flush(&lol);
-  // printf("Printed X\n");
-  // Pixel p = {0, 0, 0, 0};
-  // pbuf[0] = p;
-  // free(pbuf);
-
-  // bool filled[9] = {false};
-  // bool which_player[9] = {false};
-  // bool whos_turn = 0;
 
   VirtioInputEvent tablet_event;
   uint32_t tablet_x = 0;
@@ -182,9 +281,9 @@ int main() {
           if (tablet_event.code == ABS_Y)
             tablet_y = screen_rect.height * tablet_event.value / ABS_INT;
         }
-        printf("%d %d %d\n", tablet_event.type, tablet_event.code, tablet_event.value);
+        printf("T: 0x%x, C: 0x%x, V: 0x%x\n", tablet_event.type, tablet_event.code, tablet_event.value);
       } while (tablet_event.type != EV_SYN);
-      printf("POS: %d, %d\n", tablet_x, tablet_y);
+      printf("X: %d, Y: %d\n", tablet_x, tablet_y);
 
       if (filled[0] == false &&
           tablet_x < ROW_LIMIT0 &&
@@ -253,9 +352,28 @@ int main() {
       }
     }
 
-    // TODO: Calculate winning/draw condition
-    // TODO: Announce winner 
-    // TODO: Reset board and game
+    bool won = calc_win();
+
+    if (won)
+      annouce_winner();
+
+    if (!won && turn == 9)
+      annouce_draw();
+
+    if (won || turn == 9) {
+      // Reset game
+      turn = 0;
+      memset(&filled, 0, sizeof(filled));
+      memset(&which_player, 0, sizeof(which_player));
+      whos_turn = 0;
+      who_won = 0;
+  
+      Pixel background_pix = {0, 0, 0, 0}; // Black
+      Pixel border_pix = {0, 0, 255, 0}; // Blue
+      printf("Clearing board...\n");
+      draw_board(buf, &background_pix, &border_pix, WIDTH, HEIGHT);
+    }
+
     // sleep(10);
   }
 }
